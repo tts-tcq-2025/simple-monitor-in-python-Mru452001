cae52f55fb888209e36c3d8571593de5dd6ab853
Mru452001 <muttuh1045@gmail.com>
2025-07-20 19:33:48 +0530

    Update check_limits.py
    new_analysis

--- check_limits.py
+++ check_limits.py
@@ -1,7 +1,13 @@
-
+def is_temp_ok(temperature):
+  return 0 <= temperature <= 45
+def is_soc_ok(soc):
+  return 20 <= soc <= 80
+def is_change_rate_ok(charge_rate):
+  return charge_rate <= 0.8
+  
 def battery_is_ok(temperature, soc, charge_rate):
-  return (0 <= temperature <= 45) and (20 <= soc <= 80) and (charge_rate <= 0.8)
-
+  return is_temp_ok(temperature) and is_soc_ok(soc) and is_change_rate_ok(charge_rate)
+  
 
 if __name__ == '__main__':
   assert(battery_is_ok(25, 70, 0.7) is True)
